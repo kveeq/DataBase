f02cd704ee52889b6298d47b302dd9105240d0ce
ilsga <ilsga@DESKTOP-OS1FU9G>
2021-04-12 14:12:16 +0300

    Добавьте GITIGNORE- и GITATTRIBUTES-файлы.

---
+++ DataBase/DataBase/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBase
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Next_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btn_split_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Button_Click_2(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_3(object sender, EventArgs e)
+        { 
+        
+        }
+        private void Button_Click_4(object sender, EventArgs e)
+        { 
+
+        }
+    }
+}
